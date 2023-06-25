e9fa17dc14cf5a837779bf06b914acfb2fd84d01
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2023-06-25 23:46:25 +0200

    second1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <div>
+      When it comes to fitness training, one crucial aspect that often gets
+      overlooked is muscle tension. Understanding and effectively managing
+      muscle tension can significantly impact your fitness journey, from
+      maximizing muscle growth to preventing injuries. In this article, we will
+      explore the concept of muscle tension and its role in fitness training. We
+      will delve into various exercises, techniques, and strategies that can
+      help optimize muscle tension for enhanced performance and results.
+    </div>
+  );
 }
